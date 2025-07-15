a8ff58ec68a497bbc79c591ac08ed81d63032ff9
frectonz <fraol0912@gmail.com>
2025-07-15 20:54:26 +0300

    chore: big bang

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { env } from '$env/dynamic/private';
+
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from './server/db';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'sqlite'
+  }),
+  socialProviders: {
+    linkedin: {
+      clientId: env.LINKEDIN_CLIENT_ID,
+      clientSecret: env.LINKEDIN_CLIENT_SECRET
+    }
+  }
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { authClient } from '$lib/auth-client';
+
+  function signIn() {
+    authClient.signIn.social({
+      provider: 'linkedin'
+    });
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={signIn}> Sign In with LinkedIn </button>
